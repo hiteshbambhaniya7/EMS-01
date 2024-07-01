645a6ee13d45f5c76849397050860200b8d0076e
hiteshbambhaniya7 <bambhaniyahitsh7@gmail.com>
2024-07-01 11:48:59 +0530

    Basic boiler plate for backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,5 +14,16 @@ app.use(express.urlencoded({extended: true , limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//Routes Import
+import userRouter from './routes/user.routes.js'
+
+
+//Rutes Declaration
+app.use("/api/v1/users" , userRouter);
+
+
+//this will api end point
+// http://localhost:8000/api/v1/users/register
+
 export { app }
 
